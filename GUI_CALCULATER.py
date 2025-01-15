4617555dffdbdffabd376e0632d74dd63e8b91d9
ashag909 <goudaasharani86@gmail.com>
2025-01-15 22:41:35 +0530

    Update GUI_CALCULATER.py

--- GUI_CALCULATER.py
+++ GUI_CALCULATER.py
@@ -1,13 +1,10 @@
 import tkinter as tk
-
 calculation = ""
-
 def add_to_calculation(symbol):
     global calculation
     calculation += str(symbol)
     text_result.delete(1.0, "end")
     text_result.insert(1.0, calculation)
-
 def evaluate_calculation():
     global calculation
     try:
